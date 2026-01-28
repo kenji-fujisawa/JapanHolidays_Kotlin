d7366c0d904c99a3722edd45ba764abec9e45938
kenji-fujisawa <uhimania@uhimanianoMacBook-Air.local>
2026-01-28 17:54:31 +0900

    祝日ファイル更新用のスクリプト追加

---
+++ scripts/update_holidays.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+filename='../JapanHolidays/src/main/java/jp/uhimania/japanholidays/syukujitsu.kt'
+
+curl -o $filename -L https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv
+
+iconv -f Shift_JIS -t UTF-8 $filename | tr -d '\r' > $filename.tmp
+mv $filename.tmp $filename
+
+sed -i '' '1d' $filename
+
+sed -i '' 's/^/"/' $filename
+sed -i '' 's/,/" to "/' $filename
+sed -i '' 's/$/",/' $filename
+
+sed -i '' '1i\
+package jp.uhimania.japanholidays\
+\
+internal val holidays = mapOf(
+' $filename
+sed -i '' '$a\
+)
+' $filename
